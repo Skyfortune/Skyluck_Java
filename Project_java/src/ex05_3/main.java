e0e491a851621653ee259ed74eb327bbe68cfaa5
admin <admin@DESKTOP-A3NNEC1>
2022-07-19 16:39:54 +0900

    1

--- Project_java/src/ex05_3/main.java
+++ Project_java/src/ex05_3/main.java
@@ -3,7 +3,7 @@ package ex05_3;
 public class main {
 
 	public static void main(String[] args) {
-		Wom2Dollar toDollar = new Won2Dollar(1200);;
+		Won2Dollar toDollar = new Won2Dollar(1200);;
 		toDollar.run();
 	}
 
